85dbfaaec39f60555bd9180632fecf409a9fe54d
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-08-24 20:08:40 +0800

    Get random data of students and render them.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            students: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((res) => res.json())
+            .then((students) => {
+                this.setState({ students });
+            });
+    }
+
     render() {
         return (
-          <div>Test</div>
+            <div className="App">
+                {this.state.students.map((student) => {
+                    return (
+                        <div key={student.id}>
+                            <h1>{student.name}</h1>
+                        </div>
+                    );
+                })}
+            </div>
         );
     }
 }
