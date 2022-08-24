1e02980ce77f4a649fb950e7289297ece9aba617
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-08-24 19:05:37 +0800

    Add initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+          <div>Test</div>
+        );
+    }
+}
+
+export default App;
