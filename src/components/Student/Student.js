19304a7a7e319e00d7e666f065fa06124186edf0
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-08-25 14:38:03 +0800

    Converted student + studentlist components

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,23 +1,17 @@
-import { Component } from 'react';
 import './Student.css';
 
-class Student extends Component {
-    render() {
-        const { id, name, email } = this.props;
-        return (
-            <div className="student-card">
-                <div className="student-info">
-                    <img
-                        className="student-img"
-                        src={`https://randomuser.me/api/portraits/men/${id}.jpg`}
-                        alt="student"
-                    />
-                    <h1>{name}</h1>
-                    <p>{email}</p>
-                </div>
-            </div>
-        );
-    }
-}
+const Student = ({ id, name, email }) => (
+    <div className="student-card">
+        <div className="student-info">
+            <img
+                className="student-img"
+                src={`https://randomuser.me/api/portraits/men/${id}.jpg`}
+                alt="student"
+            />
+            <h1>{name}</h1>
+            <p>{email}</p>
+        </div>
+    </div>
+);
 
 export default Student;
--- src/components/student-list/StudentList.js
+++ src/components/student-list/StudentList.js
@@ -1,25 +1,19 @@
-import { Component } from 'react';
 import Student from '../Student/Student';
 import './StudentList.css';
 
-class StudentList extends Component {
-    render() {
-        const { students } = this.props;
-        return (
-            <div className="student-list">
-                {students.map((student) => {
-                    return (
-                        <Student
-                            name={student.name}
-                            id={student.id}
-                            key={student.id}
-                            email={student.email}
-                        />
-                    );
-                })}
-            </div>
-        );
-    }
-}
+const StudentList = ({ students }) => (
+    <div className="student-list">
+        {students.map((student) => {
+            return (
+                <Student
+                    name={student.name}
+                    id={student.id}
+                    key={student.id}
+                    email={student.email}
+                />
+            );
+        })}
+    </div>
+);
 
 export default StudentList;
