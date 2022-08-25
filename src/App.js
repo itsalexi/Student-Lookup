d40b1e6c1d3c77d7b916ecaee11ae33ec5c61a3b
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-08-25 14:35:14 +0800

    Converted App to functional

--- src/App.js
+++ src/App.js
@@ -1,57 +1,47 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
+
 import StudentList from './components/student-list/StudentList';
 import SearchBox from './components/searchbox/SearchBox';
 import Header from './components/header/Header';
 
-class App extends Component {
-    constructor() {
-        super();
+const App = () => {
+    const [searchField, setSearchField] = useState(''); // [value, setValue]
+    const [students, setStudents] = useState([]);
+    const [filteredStudents, setFilteredStudents] = useState(students);
 
-        this.state = {
-            students: [],
-            searchField: '',
-        };
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((res) => res.json())
-            .then((students) => {
-                this.setState({ students });
-            });
-    }
+            .then((users) => setStudents(users));
+    }, []);
 
-    searchHandler = (event) => {
-        // Everytime the input changes, we store the search field to the state
-        // So everytime it changes, we re-render the DOM with the newly filtered data
-        const search = event.target.value;
-        this.setState({ searchField: search });
-    };
-
-    render() {
-        const { students, searchField } = this.state;
-        const { searchHandler } = this;
-
-        // Everytime we re-render the app, we filter through the students array.
-        let filtered = students.filter((student) => {
+    useEffect(() => {
+        let newFilteredStudents = students.filter((student) => {
             return student.name
                 .toLowerCase()
                 .includes(searchField.toLowerCase());
         });
 
-        return (
-            <div className="App">
-                <Header className="header">
-                    <h1 className="title">Student Lookup</h1>
-                    <SearchBox
-                        changeHandler={searchHandler}
-                        placeholder="Find student"
-                    />
-                </Header>
-                <StudentList students={filtered} />
-            </div>
-        );
-    }
-}
+        setFilteredStudents(newFilteredStudents);
+    }, [students, searchField]);
 
+    const searchHandler = (event) => {
+        const search = event.target.value;
+
+        setSearchField(search);
+    };
+
+    return (
+        <div className="App">
+            <Header className="header">
+                <h1 className="title">Student Lookup</h1>
+                <SearchBox
+                    changeHandler={searchHandler}
+                    placeholder="Find student"
+                />
+            </Header>
+            <StudentList students={filteredStudents} />
+        </div>
+    );
+};
 export default App;
